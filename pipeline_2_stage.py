e760dac3e250b2f2d186e31985e654725d86d119
An Minh Hùng <35797529+anminhhung@users.noreply.github.com>
2021-11-10 14:28:31 +0700

    update config

--- pipeline_2_stage.py
+++ pipeline_2_stage.py
@@ -18,25 +18,27 @@ from vietocr.tool.config import Cfg
 from source.detect.mask_rcnn import detect_text_area
 from utils.visualize import draw_text_bbox
 from utils.helper import crop_text_area, create_output_file
+from configs.config import init_config
 
 # Vietocr
+CFG = init_config()
 config = Cfg.load_config_from_name('vgg_transformer')
-config['weights'] = "models/vietocr/transformerocr.pth"
-config['cnn']['pretrained']=False
-config['device'] = 'cuda:0' 
-config['predictor']['beamsearch']=False
+config['weights'] = CFG['vietocr']['weights']
+config['cnn']['pretrained'] = False
+config['device'] = CFG['system']['device']
+config['predictor']['beamsearch'] = False
 recognition_model = Predictor(config)
 
 # our dataset has two classes only - background and text
 num_classes=2
-device = torch.device("cuda")
+device = torch.device(CFG['system']['device'])
 
 backbone = resnext50_32x4d(pretrained=True, progress=True, norm_layer=misc_nn_ops.FrozenBatchNorm2d)
 backbone = BackboneWithFPN(backbone = backbone, return_layers = {'layer1': '0', 'layer2': '1', 'layer3': '2', 'layer4': '3'}, 
                              in_channels_list=[256, 512, 1024, 2048], out_channels=256, extra_blocks=LastLevelMaxPool())
 
 detect_model = MaskRCNN(backbone, num_classes).to(device)
-detect_model.load_state_dict(torch.load("models/mask_rcnn/resnext50.pth")) 
+detect_model.load_state_dict(torch.load(CFG['mask_rcnn']['model_path'])) 
 
 def predict_image(image_path):
     image = cv2.imread(image_path)
