9b052bb51cffcbd1b1812930946b4384d5ebdb1e
quoccuonglqd <quoccuonglqd123@gmail.com>
2021-12-17 20:41:04 +0700

    turn back to use only resnext50

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ gdown --id 1S5SCobkYG5geuJZ7H_SzpaZ6FY8ZgRpY
 gdown --id 112dP9jSh6VMw_zpGidjCoXld_2kBlhlp
 # resnext
 gdown --id 1Olv4QZMjkhCRKSUb3lyzFTdUyUwABgTm
+# MaskRCNN backbone python file
+gdown --id 1oqJ3qCRvWlHBUHMMy27JQxUVGa-6tWGU
 # TestA
 gdown --id 1sUqBG2mTNIovZ_zBUj37A1TtCTzSUEzs
 unzip TestA.zip 
@@ -13,5 +15,6 @@ unzip TestA.zip
 # move model 
 mv efficientb7_fail.pth models/
 mv resnext50_fail.pth models/
+mv resnet101_crop.pth models/
 mv transformerocr.pth models/vietocr
 mv TestA data
